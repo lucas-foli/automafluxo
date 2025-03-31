972e72d0423a7da79b6f45b57cedbb217d3baad1
lucas-foli <lucas.defoliveira@gmail.com>
2025-03-31 17:32:29 -0300

    adding env vars; updating login calls

--- public/fb-login.js
+++ public/fb-login.js
@@ -65,13 +65,75 @@ function updateUserInstagramId({ fbUserId, name }) {
 
 document.addEventListener('DOMContentLoaded', function () {
     const instagramLoginButton = document.getElementById('instagram-login');
+    const authorizeButton = document.getElementById('authorize');
 
-    if (instagramLoginButton) {
-        instagramLoginButton.addEventListener('click', function () {
-            console.log('Instagram login button clicked');
-            window.location.href = 'https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=1138546020791182&redirect_uri=https://automafluxo.com.br/&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish%2Cinstagram_business_manage_insights';
-        });
-    } else {
-        console.error('Instagram login button not found in the DOM');
-    }
+    instagramLoginButton.addEventListener('click', () => {
+        console.log('Instagram login button clicked');
+        window.location.href = 'https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=' + process.env.INSTAGRAM_CLIENT_ID + '&redirect_uri=' + process.env.INSTAGRAM_REDIRECT_URI + '&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish%2Cinstagram_business_manage_insights';
+    });
+    authorizeButton.addEventListener('click', () => {
+        const code = window.location.href.split('code=')[1].split('#_')[0]
+        console.log('Authorize button clicked');
+        fetchInstagramAccessToken(code);
+
+    })
 });
+const fetchInstagramAccessToken = async (code) => {
+    const url = 'https://api.instagram.com/oauth/access_token';
+    const baseUrl = process.env.INSTAGRAM_REDIRECT_URI;
+
+    const formData = new URLSearchParams();
+    formData.append('client_id', process.env.INSTAGRAM_CLIENT_ID);
+    formData.append('client_secret', process.env.INSTAGRAM_CLIENT_SECRET);
+    formData.append('grant_type', 'authorization_code');
+    formData.append('redirect_uri', baseUrl);
+    formData.append('code', code);
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: formData.toString(),
+        });
+
+        if (!response.ok) {
+            console.error('Error fetching access token:', error);
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const longToken = await getLongAccessToken(data)
+        // use('instagram_connections')
+        // db.getCollection('tokens').insert({
+        //     'userId': data.user_id,
+        //     'shortToken': data.access_token,
+        //     'longToken': longToken 
+        // })
+        window.alert('User connected!')
+    } catch (error) {
+        console.error('Error fetching access token:', error);
+    }
+};
+
+const getLongAccessToken = async (data) => {
+    const url = new URL('https://graph.instagram.com/access_token')
+    url.searchParams.append('grant_type', 'ig_exchange_token')
+    url.searchParams.append('client_secret', process.env.INSTAGRAM_CLIENT_SECRET);
+    url.searchParams.append('access_token', data.access_token)
+    try {
+        const response = await fetch(url)
+        if (!response.ok) {
+            console.error('Error fetching access token:', error);
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log('Access Token Response:', data);
+
+        return data;
+    }
+    catch (error) {
+        console.error('Error fetching access token:', error);
+    }
+}
