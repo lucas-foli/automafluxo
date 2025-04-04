cfb84b11b4fa5c9eced0b3001d5fe9d6324e8d97
lucas-foli <lucas.defoliveira@gmail.com>
2025-04-04 15:14:07 -0300

    updating build steps

--- scripts/build.js
+++ scripts/build.js
@@ -1,13 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 
-// Limpa a pasta dist (se existir)
-fs.rmSync(distDir, { recursive: true, force: true });
-
 // Diretório de origem e destino
 const publicDir = path.join(__dirname, "../public");
 const distDir = path.join(__dirname, "../dist");
 
+// Limpa a pasta dist (se existir)
+fs.rmSync(distDir, { recursive: true, force: true });
 
 // Copia a pasta public para dist
 fs.cpSync(publicDir, distDir, { recursive: true });
@@ -47,4 +46,3 @@ const processDirectory = (dir) => {
 processDirectory(distDir);
 
 console.log("✅ Build completo, todos os arquivos foram processados.");
-console.dir(distDir)
