85000b56e1a33758ff3602940c44a80d18be37f5
lucas-foli <lucas.defoliveira@gmail.com>
2025-04-14 11:48:20 -0300

    updating fetch

--- docs/scripts/instagram-login.js
+++ docs/scripts/instagram-login.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   instagramLoginButton.addEventListener(
     "click",
-    () => (window.location.href = `/api/instagram/initiate`)
+    () => (window.location.href = window.location.hostname.includes('localhost') ? 'http://localhost:3000/api/instagram/initiate' : 'https://api.automafluxo.com.br/api/instagram/initiate')
   );
   authorizeButton.addEventListener("click", () => {
     const code = window.location.href.split("code=")[1].split("#_")[0];
