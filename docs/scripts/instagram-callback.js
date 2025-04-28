c7f27d2e030b5e0b94524b7442b0ed5b26395e6d
lucas-foli <lucas.defoliveira@gmail.com>
2025-04-28 14:12:38 -0300

    replacing innerHtml with append

--- docs/scripts/instagram-callback.js
+++ docs/scripts/instagram-callback.js
@@ -15,7 +15,9 @@
   }
 
   try {
-    const response = await fetch(`https://api.automafluxo.com.br/api/instagram/callback?code=${code}`);
+    const response = await fetch(
+      `https://api.automafluxo.com.br/api/instagram/callback?code=${code}`
+    );
 
     if (!response.ok) {
       const error = await response.json();
@@ -24,7 +26,10 @@
       loginButton.hidden = false;
       loginButton.addEventListener(
         "click",
-        () => (window.location.href = window.location.hostname.includes('localhost') ? 'http://localhost:3000/api/instagram/initiate' : 'https://api.automafluxo.com.br/api/instagram/initiate')
+        () =>
+          (window.location.href = window.location.hostname.includes("localhost")
+            ? "http://localhost:3000/api/instagram/initiate"
+            : "https://api.automafluxo.com.br/api/instagram/initiate")
       );
 
       return;
@@ -35,7 +40,7 @@
     userIdParagraph.hidden = false;
     tokenParagraph.hidden = false;
     connectedParagraph.hidden = false;
-    
+
     const img = document.createElement("img");
     img.src = result.profilePictureUrl;
     img.alt = "Profile Picture";
@@ -43,13 +48,17 @@
     img.style.height = "50px"; // Set the height of the image
     img.style.borderRadius = "50%"; // Make it circular
 
-    usernameHeader.innerHTML = `Hello `;
-    usernameHeader.append(img); // Append the image to the header
-    usernameHeader.innerHTML += `@${result.username}!`;
-    
+    const helloTxt = document.createTextNode("Hello ");
+    const usernameTxt = document.createTextNode(`@${result.username}`);
+    usernameHeader.append(helloTxt, img, usernameTxt); // Append the image to the header
+
+    usernameHeader.style.display = "flex";
+    usernameHeader.style.alignItems = "center";
+    usernameHeader.style.gap = "8px"; // space between picture and text
+
     connectedParagraph.textContent = `You can start using the features of Automafluxo`;
     userIdParagraph.textContent = `Your user ID is ${result.userId}.`;
-    
+
     statusEl.textContent = "Instagram account connected successfully!";
 
     // Optional redirect after a few seconds:
@@ -59,7 +68,10 @@
     loginButton.hidden = false;
     loginButton.addEventListener(
       "click",
-      () => (window.location.href = window.location.hostname.includes('localhost') ? 'http://localhost:3000/api/instagram/initiate' : 'https://api.automafluxo.com.br/api/instagram/initiate')
+      () =>
+        (window.location.href = window.location.hostname.includes("localhost")
+          ? "http://localhost:3000/api/instagram/initiate"
+          : "https://api.automafluxo.com.br/api/instagram/initiate")
     );
   }
 })();
